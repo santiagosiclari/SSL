64ade4e9bf4d58b08070ba1ae2c8668b77b362dd
Santiago Nicolas Siclari <ssiclari@frba.utn.edu.ar>
2025-04-14 16:22:01 +0000

    Correction salida del archivo

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
+    FILE *archivo_salida = fopen("output.txt", "w");
     printf("Hola Santiago Siclari!");
+    fprintf(archivo_salida,"Hola Santiago Siclari!");
+    fclose(archivo_salida);
     return 0;
 }
