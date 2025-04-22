467dbf662032263e905c7df2403d6afa053e9293
Santiago Nicolas Siclari <ssiclari@frba.utn.edu.ar>
2025-04-22 16:52:33 -0300

    Correction punto 4.c.2

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
 int main() {
-    FILE *archivo_salida = fopen("output.txt", "w");
     printf("Hola Santiago Siclari!");
-    fprintf(archivo_salida,"Hola Santiago Siclari!");
-    fclose(archivo_salida);
     return 0;
 }
+/* Ejecucion en la consola
+    gcc hello.c -o hello
+    ./hello
+    Para redireccionar la salida del archivo hello.c:
+    ./hello > output.txt
+*/
